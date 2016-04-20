3f12a3c18e2f8f79bfb944178291c524f54559da
Nattanich <nattanichpoon@gmail.com>
2016-04-21 01:42:09 +0700

    new navbar

--- src/static_in_pro/our_static/js/myscript.js
+++ src/static_in_pro/our_static/js/myscript.js
@@ -1,7 +1,31 @@
-$('#project_filters > ul.nav-pills li').click(function(e) {
+
+
+jQuery(document).ready(function() {
+  if ($('.cd-stretchy-nav').length > 0) {
+    var stretchyNavs = $('.cd-stretchy-nav');
+
+    stretchyNavs.each(function() {
+      var stretchyNav = $(this),
+        stretchyNavTrigger = stretchyNav.find('.cd-nav-trigger');
+
+      stretchyNavTrigger.on('click', function(event) {
+        event.preventDefault();
+        stretchyNav.toggleClass('nav-is-visible');
+      });
+    });
+
+    $(document).on('click', function(event) {
+      (!$(event.target).is('.cd-nav-trigger') && !$(event.target).is('.cd-nav-trigger span')) && stretchyNavs.removeClass('nav-is-visible');
+    });
+
+  }
+
+
+  $('#project_filters > ul.nav-pills li').click(function(e) {
     $('.nav li.active').removeClass('active');
     var $this = $(this);
     $this.addClass('active');
     e.preventDefault();
+});
 });
 

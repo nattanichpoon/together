c06f181071bf8e63c4cfa54d3dfa66d639dcea1e
Nattanich <nattanichpoon@gmail.com>
2016-04-20 16:49:25 +0700

    Revert "added schedule model"
    This reverts commit c2f6c713103ed9504cc4ee044768899ed6166518.

--- src/schedule/models.py
+++ src/schedule/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 # Create your models here.
 # Create your models here.
-
 class Date(models.Model):
 	date = models.IntegerField()
 	month = models.CharField(max_length=250)
@@ -12,12 +11,6 @@ class Date(models.Model):
 class Album(models.Model):
 	artist = models.CharField(max_length=250)
 	album_title = models.CharField(max_length=500)
-
-	#primary/unique key; each album has one key
-	#Red:pk=1
-
-	#each song is linked to an album
-	#fk=1 (so we know this song belongs to red)
  #primary/unique key; each album has one key
  #Red:pk=1
 

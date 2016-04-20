2ac7f03a165b2e8c6e091831657f775ed91be9cd
nainachugh <nainachugh@Nainas-MacBook-Air.local>
2016-04-20 14:52:18 +0700

    test

--- src/schedule/models.py
+++ src/schedule/models.py
@@ -3,3 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Date(models.Model):
+	date = models.IntegerField()
+	month = models.CharField(max_length=250)
+
+class Album(models.Model):
+	artist = models.CharField(max_length=250)
+	album_title = models.CharField(max_length=500)
+	#primary/unique key; each album has one key
+	#Red:pk=1
+
+class Song(models.Model):
+	album = models.ForeignKey(Album, on_delete=models.CASCADE)
+	#each song is linked to an album
+	#fk=1 (so we know this song belongs to red)

edeb5dbaeedcc86275cde3dc413f98559b4a03a4
Nattanich <nattanichpoon@gmail.com>
2016-04-20 16:50:57 +0700

    p

--- src/schedule/admin.py
+++ src/schedule/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from schedule.models import Date, Album, Song
 
 # Register your models here.
-admin.site.register(Date)
-admin.site.register(Album)
-admin.site.register(Song)
+# admin.site.register(Date)
+# admin.site.register(Album)
+# admin.site.register(Song)
 

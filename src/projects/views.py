fe06365c89f9db2e8105ad990073404dc1b076e4
Nattanich <nattanichpoon@gmail.com>
2016-04-27 11:11:58 +0700

    resolved conflicts

--- src/projects/views.py
+++ src/projects/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
 from django.conf import settings
-<<<<<<< HEAD
 from users.models import UserProfile
-from projects.models import Project
 import datetime
 
-=======
 from .forms import ProjectForm
-from users.models import UserProfile
+
 from projects.models import Project, Task
->>>>>>> p
 
 # Create your views here.
 def myprojects(request):
@@ -32,7 +28,6 @@ def myprojects(request):
 	}
 	return render(request, "myprojects.html", context)
 
-<<<<<<< HEAD
 def mytasks(request):
 	title = "My Projects"
 	title_align_center = True
@@ -52,7 +47,7 @@ def mytasks(request):
 	"today":today,
 	}
 	return render(request, "mytasks.html", context)
-=======
+
 def project_new(request):
 	if request.method == "POST":
 		form = ProjectForm(request.POST)
@@ -67,4 +62,3 @@ def project_new(request):
 
 		
 	return render(request, "project_new.html", {"form":form})
->>>>>>> p

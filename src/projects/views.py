1cf287f93cb4e0db7eab9a1f9faf5d191ddf67a2
Archaree <archareem@gmail.com>
2016-05-06 17:26:50 +0700

    working on task detail

--- src/projects/views.py
+++ src/projects/views.py
@@ -25,39 +25,24 @@ def myprojects(request):
 	"today":today,
 	}
 	return render(request, "myprojects.html", context)
+
 def project_detail(request,pk):
 	project = get_object_or_404(Project, pk=pk)
 	members = project.members.all()
 
+	allTasks = Task.objects.order_by('-expectedDate').filter(project=project).all()
 
 	context ={
 		'project': project,
-		'members': members
+		'members': members,
+		'tasks': allTasks,
 	}
-	
 
 	return render(request, 'project_detail.html', context)
 
-def mytasks(request):
-	title = "My Projects"
-	title_align_center = True
-	navtab = True
-
-	profile = UserProfile.objects.get(username=request.user)
-	projects = Project.objects.filter(members__username=request.user.username)
-	today = datetime.datetime.today()
-	task
-
-
-	context = {
-	"title": title,
-	"title_align_center":title_align_center,
-	"navtab":navtab,
-	"profile":profile,
-	"projects":projects,
-	"today":today,
-	}
-	return render(request, "mytasks.html", context)
+def task_detail(request, pk):
+	task = get_object_or_404(Task, pk=pk)
+	return render(request, 'task_detail.html', {'task': task})
 
 def project_new(request):
 	if request.method == "POST":

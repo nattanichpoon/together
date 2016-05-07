d243e5f79efba36fe1a6c5cf2a6140f8cf3b40c9
Nattanich <nattanichpoon@gmail.com>
2016-05-07 14:18:48 +0700

    migrated

--- src/schedule1/models.py
+++ src/schedule1/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Date(models.Model):
@@ -12,12 +13,33 @@ class Date(models.Model):
 class Album(models.Model):
 	artist = models.CharField(max_length=250)
 	album_title = models.CharField(max_length=500)
+
 	def __str__(self):
 		return self.album_title
- #primary/unique key; each album has one key
- #Red:pk=1
+
+	# primary/unique key; each album has one key
+	# Red:pk=1
 
 class Song(models.Model):
 	album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
 	def __str__(self):
 		return self
+
+class Meeting(models.Model):
+	project = models.ForeignKey('projects.Project')
+	meetingName = models.CharField(max_length=500)
+	meetingAgenda = models.CharField(max_length=1000)
+	meetingSummary = models.CharField(max_length=1000)
+	meetingDate = models.DateField(default=timezone.now)
+	CONFIRM = 'CF'
+	REJECT = 'RJ'
+	STATUS_CHOICES=((CONFIRM,'Confirm'),(REJECT,'Reject'))
+	meetingStatus=models.CharField(choices=STATUS_CHOICES, max_length=200)
+	confirmedCount = models.IntegerField();
+
+	def get_status(self):
+		return self.meetingStatus
+
+	def __str__(self):
+		return self.taskName

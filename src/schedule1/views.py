0c9e31c56a6821353397b23470ec02e310a85e35
Nattanich <nattanichpoon@gmail.com>
2016-05-08 14:18:49 +0700

    tried adding nans stuff

--- src/schedule1/views.py
+++ src/schedule1/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.conf import settings
+from projects.models import Project
 
 
 # Create your views here.
@@ -14,3 +15,27 @@ def myschedule(request):
     "navtab":navtab,
     }
     return render(request, "myschedule.html", context)
+
+
+def schedule_shared(request):
+    title = "Shared Schedule"
+    title_align_center = True
+    navtab = True
+
+    context = {
+    "title": title,
+    "title_align_center":title_align_center,
+    "navtab":navtab,
+    }
+    return render(request, "schedule_shared.html", context)
+
+
+
+#def project_detail(request,pk):
+#	project = get_object_or_404(Project, pk=pk)
+#	members = project.members.all()
+#	context ={
+#		'project': project,
+#		'members': members
+#	}
+#	return render(request, 'project_detail.html', context)
